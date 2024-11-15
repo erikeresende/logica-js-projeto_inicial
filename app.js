4fe4cdee53224adf77d1b8a2e75591930823b163
Érike Resende <erike2000augusto@gmail.com>
2024-11-15 13:30:05 -0300

    Clear

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let chute;
 let tentativas = 1;
 
-
 //enquanto chute não for igual ao número secreto.
 while (chute != numeroSecreto) {
 
@@ -22,31 +21,8 @@ while (chute != numeroSecreto) {
         }
         // tentativas = tentativas + 1;
         tentativas ++;
-        // alert (`Você errou, o número secreto era ${numeroSecreto} e você escolheu ${chute} :(`)
     }
 }
 
 let palavraTentativa = tentativas > 1 ? 'Tentativas' : 'tentativa'
 alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
-// if (tentativas > 1) {
-// alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas.`);
-// } else {
-//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa.`);
-// }
-
-
-
-// alert('Boas vindas ao nosso site!');
-// let nome = prompt ('Usuário');
-// let idade = prompt ('Qual a sua idade?');
-// let numeroDeVendas = 50;
-// let saldoDisponível = 1000;
-// let mensagemDeErro = ('Erro! Preenha todos os campos')
-
-// if (idade >= 18) {
-//     alert (' Pode tirar a habilitação')
-// }
-
-// else
-//     alert ('Não pode tirar a habilitação')
-
